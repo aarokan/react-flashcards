bc189f4c47fdfc16fceffbc2a6b6e09139707db5
Abdullah <aarokan@gmail.com>
2019-12-27 07:35:00 +0300

    notify the user

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,36 +1,49 @@
-export function getDecksInfo (deck) {
-  info = {
-    HTML: {
-      title: 'HTML',
-      questions: [
-        {
-          question: 'What is HTML?',
-          answer: 'Hypertext Markup Language'
-        },
-        {
-          question: 'What is a marquee?',
-          answer: 'A marquee allows you to put a scrolling text in a web page'
-        },
-      ]
-    },
-    React: {
-      title: 'React',
-      questions: [
-        {
-          question: 'What is React?',
-          answer: 'A JavaScript library for building user interfaces'
-        },
-        {
-          question: 'What is JSX?',
-          answer: 'JSX is a shorthand for JavaScript XML'
-        },
-        {
-          question: 'How can you update the state of a component?',
-          answer: 'State of a component can be updated using this.setState()'
-        },
-      ]
-    }
-  }
+import {AsyncStorage} from 'react-native';
+import {Notifications, Permissions} from 'expo';
 
-  return typeof deck === 'undefined' ? info : info[deck]
+const NOTIFICATION_KEY = 'notification:udacicards';
+
+function createNotification() {
+  return {
+    title: 'Udacicards App',
+    body: "Don't forget to have a quiz today!",
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      vibrate: true
+    },
+  };
+}
+
+export function setNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then(data => {
+      if (!data) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+        .then(({status}) => {
+          if (status === 'granted') {
+            Notifications.cancelAllScheduledNotificationsAsync()
+
+              let today = new Date();
+              today.setDate(today.getDate());
+              today.setHours(20);
+              today.setMinutes(0);
+
+              Notifications.scheduleLocalNotificationAsync(
+                  createNotification(), 
+                  {
+                    time: today,
+                    repeat: 'day',
+                  }
+              )
+
+            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+          }
+        })
+      }
+    })
 }
